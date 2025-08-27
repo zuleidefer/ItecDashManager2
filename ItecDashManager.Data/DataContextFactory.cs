c90e3547e87fa630d1c8ef473ae1541f807deee3
zuleidefer <aneveszuleide@gmail.com>
2025-08-27 15:38:28 -0300

    Adiciona tabela users

---
+++ ItecDashManager.Data/DataContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ItecDashManager.Data.Context
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+               
+                connectionString = "Host=127.0.0.1;Port=5432;Database=postgres;Username=postgres;Password=novobanco123;Minimum Pool Size=10;";
+            }
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
